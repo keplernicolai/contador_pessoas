95ec04f7839912ab6092908c66ccb76d881105cf
keplernicolai <keplernicolai@gmail.com>
2021-07-04 01:46:29 -0300

    Imagem, setState de botão e textos

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,95 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _textInfo = "Pode entrar";
+
+  void _mudarPessoa(int quant) {
+    setState(() {
+      _people += quant;
+
+      if (_people < 0) {
+        _textInfo = "Mundo invertido!";
+      } else if (_people <= 10) {
+        _textInfo = "Pode entrar!";
+      } else {
+        _textInfo = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pessoas: $_people",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextButton(
+                        onPressed: () {
+                          _mudarPessoa(1);
+                        },
+                        child: Text(
+                          "+1",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40.0,
+                              color: Colors.white),
+                        ))),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                      onPressed: () {
+                        _mudarPessoa(-1);
+                      },
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40.0,
+                            color: Colors.white),
+                      )),
+                ),
+              ],
+            ),
+            Text(
+              _textInfo,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
